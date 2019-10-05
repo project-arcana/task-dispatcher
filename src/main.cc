9e236a9f47a904170f9f875bbbf1b39e77558434
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-05 04:49:19 +0200

    Continue experiments in main

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <array>
 #include <iostream>
 #include <td.hh>
 
@@ -16,11 +17,71 @@ void execute(F&& func, FObj& inst, Args&&... args)
     (inst.*func)(args...);
 }
 
+int calculate_fibonacci(int n)
+{
+    if (n < 2)
+        return n;
+    else
+    {
+        auto f1 = td::submit(calculate_fibonacci, n - 1);
+        auto f2 = td::submit(calculate_fibonacci, n - 2);
+        return f1.get_unpinned() + f2.get_unpinned();
+    }
+}
+
+double fac(double num)
+{
+    double result = 1.0;
+    for (double i = 2.0; i < num; i++)
+        result *= i;
+    return result;
+}
+
+double chudnovsky(double k_start, double k_end)
+{
+    auto res = 0.0;
+    for (double k = k_start; k < k_end; k++)
+    {
+        res += (pow(-1.0, k) * fac(6.0 * k) * (13591409.0 + (545140134.0 * k))) / (fac(3.0 * k) * pow(fac(k), 3.0) * pow(640320.0, 3.0 * k + 3.0 / 2.0));
+    }
+    return res * 12.0;
+}
+
+double calculate_pi(int k, int num_batches_target)
+{
+    auto batch_size = td::int_div_ceil(k, num_batches_target);
+    auto num_batches = td::int_div_ceil(k, batch_size);
+
+    auto batchStorage = new double[num_batches];
+    for (auto i = 0; i < num_batches; ++i)
+        batchStorage[i] = 0.0;
+
+    auto sync = td::submit_n([batchStorage, batch_size] (auto i) {
+
+        auto k_start = double(i * batch_size);
+        auto k_end = k_start + double(batch_size);
+        batchStorage[i] = chudnovsky(k_start, k_end);
+
+    }, num_batches);
+
+    td::wait_for_unpinned(sync);
+
+    auto res = 0.0;
+    for (auto i = 0; i < num_batches; ++i)
+        res += batchStorage[i];
+
+    delete[] batchStorage;
+
+    return 1.0 / res;
+}
+
 }
 
 int main()
 {
-    td::launch([] {
+    td::scheduler_config config;
+    //   config.num_threads = 1;
+    td::launch(config, [] {
         auto s1 = td::submit([] { printf("Task 1\n"); });
         td::submit(s1, [] { printf("Task 1 - append \n"); });
         td::submit(s1, argfun, 1, 2, 3);
@@ -31,8 +92,10 @@ int main()
 
         td::wait_for_unpinned(s1);
 
-        auto future1 = td::submit([] { return 5.f * 15.f; });
-        future1.get_unpinned();
+        auto f1 = td::submit([] { return 5.f * 15.f; });
+        std::cout << "Future 1: " << f1.get_unpinned() << std::endl;
+
+        std::cout << "PI: " << calculate_pi(1000, 64) << std::endl;
 
         td::sync s2;
 
