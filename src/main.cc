f622a79742360ab946f60e4abebebf467ff929fa
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-06 19:05:36 +0200

    Add small benchmark

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,8 @@
 #include <array>
-#include <iostream>
-
 #include <container/spmc_queue.hh>
+#include <iostream>
 #include <td.hh>
+#include <common/math_intrin.hh>
 
 namespace
 {
@@ -58,13 +58,13 @@ double calculate_pi(int k, int num_batches_target)
     for (auto i = 0; i < num_batches; ++i)
         batchStorage[i] = 0.0;
 
-    auto sync = td::submit_n([batchStorage, batch_size] (auto i) {
-
-        auto k_start = double(i * batch_size);
-        auto k_end = k_start + double(batch_size);
-        batchStorage[i] = chudnovsky(k_start, k_end);
-
-    }, num_batches);
+    auto sync = td::submit_n(
+        [batchStorage, batch_size](auto i) {
+            auto k_start = double(i * batch_size);
+            auto k_end = k_start + double(batch_size);
+            batchStorage[i] = chudnovsky(k_start, k_end);
+        },
+        num_batches);
 
     td::wait_for_unpinned(sync);
 
@@ -81,65 +81,92 @@ double calculate_pi(int k, int num_batches_target)
 
 int main()
 {
-    auto [wrk, stl] = td::container::make_spmc_queue<int>();
-    wrk.push(15);
-    wrk.push(62);
-    std::cout << "got " << wrk.pop().value_or(-1) << std::endl;
-    std::cout << "stole " << stl.steal().value_or(-1) << std::endl;
-
     td::scheduler_config config;
-    //   config.num_threads = 1;
-    td::launch(config, [] {
-        auto s1 = td::submit([] { printf("Task 1\n"); });
-        td::submit(s1, [] { printf("Task 1 - append \n"); });
-        td::submit(s1, argfun, 1, 2, 3);
+//    config.max_num_jobs = 1000000;
+    td::launch(config, [&] {
+        if(0)
+        {
+            auto s1 = td::submit([] { printf("Task 1\n"); });
+            td::submit(s1, [] { printf("Task 1 - append \n"); });
+            td::submit(s1, argfun, 1, 2, 3);
 
-        // TODO
-        //        Foo f;
-        //        td::submit(s1, &Foo::process_args, f, 15, 16);
+            // TODO
+            //        Foo f;
+            //        td::submit(s1, &Foo::process_args, f, 15, 16);
 
-        td::wait_for_unpinned(s1);
+            td::wait_for_unpinned(s1);
+        }
 
-        auto f1 = td::submit([] { return 5.f * 15.f; });
-        std::cout << "Future 1: " << f1.get_unpinned() << std::endl;
+        for (auto _ = 0; _ < 10; ++_)
+        {
+            auto largeTaskAmount = new td::container::Task[config.max_num_jobs];
+            for (auto i = 0u; i < config.max_num_jobs; ++i)
+                largeTaskAmount[i].lambda([] { /* SWYM */ });
 
-        std::cout << "PI: " << calculate_pi(1000, 64) << std::endl;
+            auto before = td::intrin::rdtsc();
+            auto s = td::submit_raw(largeTaskAmount, config.max_num_jobs);
+            auto after = td::intrin::rdtsc();
 
-        td::sync s2;
+            std::cout << "Average dispatch time: " << (after - before) / double(config.max_num_jobs) << " cycles" << std::endl;
 
-        td::submit(s2, [] {
-            printf("Task 2 start \n");
+            delete[] largeTaskAmount;
 
-            auto s2_i = td::submit_n([](auto i) { printf("Task 2 - inner %d \n", i); }, 4);
+            auto before_wait = td::intrin::rdtsc();
+            td::wait_for_unpinned(s);
+            auto after_wait = td::intrin::rdtsc();
 
-            td::wait_for_unpinned(s2_i);
+            std::cout << "Wait time: " << (after_wait - before_wait) << " cycles" << std::endl;
 
-            printf("Task 2 end \n");
-        });
+        }
 
-        td::submit(s2, [] { printf("Task 3 \n"); });
+        return;
 
-        td::wait_for_unpinned(s2);
+        {
+            auto f1 = td::submit([] { return 5.f * 15.f; });
+            std::cout << "Future 1: " << f1.get_unpinned() << std::endl;
 
-        td::sync s3;
-        td::submit_n(
-            s3,
-            [](auto i) {
-                printf("Task 4 - %d start \n", i);
+            std::cout << "PI: " << calculate_pi(1000, 64) << std::endl;
+        }
 
-                auto s4_i = td::submit_n(
-                    [i](auto i_inner) {
-                        //
-                        printf("Task 4 - %d - inner %d \n", i, i_inner);
-                    },
-                    4);
+        {
+            td::sync s2;
 
-                printf("Task 4 - %d wait \n", i);
-                td::wait_for_unpinned(s4_i);
-            },
-            4);
+            td::submit(s2, [] {
+                printf("Task 2 start \n");
 
-        td::wait_for_unpinned(s3);
+                auto s2_i = td::submit_n([](auto i) { printf("Task 2 - inner %d \n", i); }, 4);
+
+                td::wait_for_unpinned(s2_i);
+
+                printf("Task 2 end \n");
+            });
+
+            td::submit(s2, [] { printf("Task 3 \n"); });
+
+            td::wait_for_unpinned(s2);
+        }
+
+        {
+            td::sync s3;
+            td::submit_n(
+                s3,
+                [](auto i) {
+                    printf("Task 4 - %d start \n", i);
+
+                    auto s4_i = td::submit_n(
+                        [i](auto i_inner) {
+                            //
+                            printf("Task 4 - %d - inner %d \n", i, i_inner);
+                        },
+                        4);
+
+                    printf("Task 4 - %d wait \n", i);
+                    td::wait_for_unpinned(s4_i);
+                },
+                4);
+
+            td::wait_for_unpinned(s3);
+        }
     });
 
 
