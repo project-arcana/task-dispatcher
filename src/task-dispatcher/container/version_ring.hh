a5800b3e07bb4ba2b24501aec6c5b3de4011c45f
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-08 18:53:27 +0200

    Add comments to VersionRing

--- src/task-dispatcher/container/version_ring.hh
+++ src/task-dispatcher/container/version_ring.hh
@@ -9,7 +9,8 @@ namespace td::container
 template <class T, unsigned N>
 struct VersionRing
 {
-    static_assert(N < unsigned(-1) / 2, "version_ring too large");
+    static_assert(N > 0);
+    static_assert(N < unsigned(-1) / 2, "VersionRing too large");
 
 private:
     T mData[N];
@@ -23,16 +24,20 @@ private:
 public:
     explicit VersionRing() = default;
 
-    bool isExpired(unsigned handle) const { return mVersion.load(std::memory_order_acquire) - handle > N; }
+    /// Returns true if the handle no longer refers to a valid slot on the ring buffer
+    [[nodiscard]] bool isExpired(unsigned handle) const { return mVersion.load(std::memory_order_acquire) - handle > N; }
 
-    unsigned acquire(T value)
+    /// Acquires a slot in the ring buffer, writing a value to it
+    [[nodiscard]] unsigned acquire(T value)
     {
-        auto handle = mVersion.fetch_add(1, std::memory_order_acquire);
+        auto const handle = mVersion.fetch_add(1, std::memory_order_acquire);
         mData[handle % N] = std::move(value);
         return handle;
     }
 
-    T& get(unsigned handle) { return mData[handle % N]; }
+    /// Accesses the slot in the ring buffer
+    [[nodiscard]] T& get(unsigned handle) { return mData[handle % N]; }
+    [[nodiscard]] T const& get(unsigned handle) const { return mData[handle % N]; }
 
     void reset() { mVersion.store(0, std::memory_order_release); }
 };
