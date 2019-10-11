0fca5963b68d5051d2224c016f026053da04871a
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-11 19:37:00 +0200

    Reorder, complete and unify td api header

--- src/task-dispatcher/td.hh
+++ src/task-dispatcher/td.hh
@@ -12,6 +12,8 @@
 
 namespace td
 {
+namespace detail
+{
 // Divide ints and round up
 // a > 0, b > 0
 template <class T = int>
@@ -19,8 +21,10 @@ constexpr T int_div_ceil(T a, T b)
 {
     return 1 + ((a - 1) / b);
 }
+}
 
-// == wait ==
+// ==========
+// Wait
 
 inline void wait_for(sync& sync) { Scheduler::current().wait(sync, true); }
 inline void wait_for_unpinned(sync& sync) { Scheduler::current().wait(sync); }
@@ -38,7 +42,8 @@ void wait_for_unpinned(STs&... syncs)
 }
 
 
-// == getter / misc ==
+// ==========
+// Getter / Miscelaneous
 
 [[nodiscard]] inline bool is_scheduler_alive() { return Scheduler::isInsideScheduler(); }
 
@@ -81,7 +86,8 @@ public:
     future& operator=(future&&) noexcept = default;
 };
 
-// == launch ==
+// ==========
+// Launch
 
 template <class F>
 void launch(scheduler_config& config, F&& func)
@@ -113,62 +119,14 @@ void launch(F&& func)
     scheduler.start(mainTask);
 }
 
-// == submit ==
+// ==========
+// Submit
 
+// Raw submit from constructed Task types
 void submit_raw(sync& sync, container::Task* tasks, unsigned num) { td::Scheduler::current().submitTasks(tasks, num, sync); }
 void submit_raw(sync& sync, cc::span<container::Task> tasks) { submit_raw(sync, tasks.data(), unsigned(tasks.size())); }
 
-[[nodiscard]] sync submit_raw(cc::span<container::Task> tasks)
-{
-    td::sync res;
-    submit_raw(res, tasks.data(), unsigned(tasks.size()));
-    return res;
-}
-
-[[nodiscard]] sync submit_raw(container::Task* tasks, unsigned num)
-{
-    td::sync res;
-    submit_raw(res, tasks, num);
-    return res;
-}
-
-
-template <class F, class... Args>
-[[nodiscard]] auto submit(F&& fun, Args&&... args)
-{
-    static_assert(std::is_invocable_v<F, Args...>, "function must be invocable with the given args");
-    using R = std::decay_t<std::invoke_result_t<F, Args...>>;
-    if constexpr (std::is_same_v<R, void>)
-    {
-        sync res;
-        container::Task dispatch([=] { fun(args...); });
-        submit_raw(res, &dispatch, 1);
-        return res;
-    }
-    else
-    {
-        sync s;
-        future<R> res;
-        R* const result_ptr = res.get_raw_pointer();
-        container::Task dispatch([=] { *result_ptr = fun(args...); });
-        submit_raw(s, &dispatch, 1);
-        res.set_sync(s);
-        return res;
-    }
-}
-
-template <class F, class... Args>
-[[nodiscard]] auto submit(sync& s, F&& fun, Args&&... args)
-{
-    static_assert(std::is_invocable_v<F, Args...>, "function must be invocable with the given args");
-    static_assert(std::is_same_v<std::invoke_result_t<F, Args...>, void>, "return must be void");
-    // A lambda callung fun(args...), but moving the args instead of copying them into the lambda
-    container::Task dispatch(
-        [fun, tup = std::make_tuple(std::move(args)...)] { std::apply([&fun](auto&&... args) { fun(decltype(args)(args)...); }, tup); });
-    submit_raw(s, &dispatch, 1);
-}
-
-// TODO
+// TODO Single pointer to member with arguments
 // template <class F, class FObj, class... Args>
 // void submit(sync& s, F&& fun, FObj& inst, Args&&... args)
 //{
@@ -179,6 +137,7 @@ template <class F, class... Args>
 //    Scheduler::current().submitTasks(&dispatch, 1, s);
 //}
 
+// Single lambda
 template <class F>
 void submit(sync& sync, F&& func)
 {
@@ -188,6 +147,19 @@ void submit(sync& sync, F&& func)
     submit_raw(sync, &dispatch, 1);
 }
 
+// Single lambda with arguments
+template <class F, class... Args>
+void submit(sync& s, F&& fun, Args&&... args)
+{
+    static_assert(std::is_invocable_v<F, Args...>, "function must be invocable with the given args");
+    static_assert(std::is_same_v<std::invoke_result_t<F, Args...>, void>, "return must be void");
+    // A lambda callung fun(args...), but moving the args instead of copying them into the lambda
+    container::Task dispatch(
+        [fun, tup = std::make_tuple(std::move(args)...)] { std::apply([&fun](auto&&... args) { fun(decltype(args)(args)...); }, tup); });
+    submit_raw(s, &dispatch, 1);
+}
+
+// Lambda called n times with index argument
 template <class F>
 void submit_n(sync& sync, F&& func, unsigned n)
 {
@@ -200,6 +172,23 @@ void submit_n(sync& sync, F&& func, unsigned n)
     delete[] tasks;
 }
 
+// Lambda called for each element with element reference
+template <class T, class F>
+void submit_each(sync& sync, F&& func, cc::span<T> vals)
+{
+    static_assert(std::is_invocable_v<F, T&>, "function must be invocable with element reference");
+    static_assert(std::is_same_v<std::invoke_result_t<F, T&>, void>, "return must be void");
+
+    auto tasks = new container::Task[vals.size()];
+
+    for (auto i = 0u; i < vals.size(); ++i)
+        tasks[i].lambda([func, val_ptr = vals.data() + i] { func(*val_ptr); });
+
+    submit_raw(sync, tasks, vals.size());
+    delete[] tasks;
+}
+
+// Lambda called for each batch, with batch start and end
 template <class F>
 void submit_batched(sync& sync, F&& func, unsigned n, unsigned num_batches_target = td::system::hardware_concurrency * 4)
 {
@@ -217,6 +206,10 @@ void submit_batched(sync& sync, F&& func, unsigned n, unsigned num_batches_targe
     delete[] tasks;
 }
 
+// ==========
+// Lambdas with return values
+
+// Lambda - sync return variant, with optional return type
 template <class F>
 [[nodiscard]] auto submit(F&& fun)
 {
@@ -241,17 +234,75 @@ template <class F>
     }
 }
 
+// Lambda with arguments - sync return variant, with optional return type
+template <class F, class... Args>
+[[nodiscard]] auto submit(F&& fun, Args&&... args)
+{
+    static_assert(std::is_invocable_v<F, Args...>, "function must be invocable with the given args");
+    using R = std::decay_t<std::invoke_result_t<F, Args...>>;
+    if constexpr (std::is_same_v<R, void>)
+    {
+        sync res;
+        container::Task dispatch([=] { fun(args...); });
+        submit_raw(res, &dispatch, 1);
+        return res;
+    }
+    else
+    {
+        sync s;
+        future<R> res;
+        R* const result_ptr = res.get_raw_pointer();
+        container::Task dispatch([=] { *result_ptr = fun(args...); });
+        submit_raw(s, &dispatch, 1);
+        res.set_sync(s);
+        return res;
+    }
+}
+
+// ==========
+// Sync return variants
+
+[[nodiscard]] sync submit_raw(cc::span<container::Task> tasks)
+{
+    td::sync res;
+    submit_raw(res, tasks.data(), unsigned(tasks.size()));
+    return res;
+}
+
+[[nodiscard]] sync submit_raw(container::Task* tasks, unsigned num)
+{
+    td::sync res;
+    submit_raw(res, tasks, num);
+    return res;
+}
+
 template <class F>
 [[nodiscard]] sync submit_n(F&& func, unsigned n)
 {
     static_assert(std::is_invocable_v<F, unsigned>, "function must be invocable with index argument");
     static_assert(std::is_same_v<std::invoke_result_t<F, unsigned>, void>, "return must be void");
     sync res;
-    auto tasks = new container::Task[n];
-    for (auto i = 0u; i < n; ++i)
-        tasks[i].lambda([=] { func(i); });
-    submit_raw(res, tasks, n);
-    delete[] tasks;
+    submit_n<F>(res, std::forward<F>(func), n);
+    return res;
+}
+
+template <class T, class F>
+[[nodiscard]] sync submit_each(F&& func, cc::span<T> vals)
+{
+    static_assert(std::is_invocable_v<F, T&>, "function must be invocable with element reference");
+    static_assert(std::is_same_v<std::invoke_result_t<F, T&>, void>, "return must be void");
+    sync res;
+    submit_each<T, F>(res, std::forward<F>(func), vals);
+    return res;
+}
+
+template <class F>
+[[nodiscard]] sync submit_batched(F&& func, unsigned n, unsigned num_batches_target = td::system::hardware_concurrency * 4)
+{
+    static_assert(std::is_invocable_v<F, unsigned, unsigned>, "function must be invocable with batch start and end argument");
+    static_assert(std::is_same_v<std::invoke_result_t<F, unsigned, unsigned>, void>, "return must be void");
+    sync res;
+    submit_batched<F>(res, std::forward<F>(func), n, num_batches_target);
     return res;
 }
 
