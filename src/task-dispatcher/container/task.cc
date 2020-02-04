66af827b2e6801bdbae7fe7f7cffa3463ec46858
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-02-04 13:24:12 +0100

    Remove unneccesary task.cc

--- src/task-dispatcher/container/task.cc
+++
@@ -1,5 +0,0 @@
-#include "task.hh"
-
-static_assert(sizeof(td::container::task) == td::system::l1_cacheline_size, "task exceeds cacheline size");
-static_assert(alignof(td::container::task) == alignof(std::max_align_t), "task risks misalignment");
-static_assert(std::is_trivial_v<td::container::task>, "task is not trivial");
--- src/task-dispatcher/container/task.hh
+++ src/task-dispatcher/container/task.hh
@@ -114,4 +114,8 @@ public:
     // Execute the contained task and clean it up afterwards (invalidates task)
     void execute_and_cleanup() { _exec_cleanup_func(_buffer); }
 };
+
+static_assert(sizeof(td::container::task) == td::system::l1_cacheline_size, "task exceeds cacheline size");
+static_assert(alignof(td::container::task) == alignof(std::max_align_t), "task risks misalignment");
+static_assert(std::is_trivial_v<td::container::task>, "task is not trivial");
 }
