fbe2989bd55cd63c1e7a5d919867c039f382a036
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2021-04-29 15:23:17 +0200

    Remove misplaced nodiscard attribute

--- src/task-dispatcher/common/spin_lock.hh
+++ src/task-dispatcher/common/spin_lock.hh
@@ -64,7 +64,7 @@ template <typename T>
 class LockGuard
 {
 public:
-    [[nodiscard]] CC_FORCE_INLINE explicit LockGuard(T& mutex) : mMutex(mutex) { mMutex.lock(); }
+    CC_FORCE_INLINE explicit LockGuard(T& mutex) : mMutex(mutex) { mMutex.lock(); }
     CC_FORCE_INLINE ~LockGuard() { mMutex.unlock(); }
 
     LockGuard(const LockGuard&) = delete;
